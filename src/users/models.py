bb260d61b4df41b98a5a898f923a466d83d02e92
Muhammed Anas M A <muhammedanas6282@gmail.com>
2025-02-13 19:51:53 +0530

    first commit

---
+++ src/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image = models.ImageField(null=True)
+    bio = models.CharField(max_length=150, blank=True)
+    phone_number = models.CharField(max_length=12, blank=True)
+    
