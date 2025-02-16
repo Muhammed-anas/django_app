12873b396ef764ab911facc0064c5d57872a48cc
Muhammed Anas M A <muhammedanas6282@gmail.com>
2025-02-16 11:15:04 +0530

    #

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'localflavor',
+    'crispy_forms',
     'main',
     'users',
     
@@ -122,7 +123,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
 
+#CRISPY FORMS SETTINGS
+# CRISPY_TEMPLATE_PACK = 'bootstarp5'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
