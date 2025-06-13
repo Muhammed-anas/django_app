5f5de834745b9c8da62490fa483a1562d1c8c8f3
Muhammed Anas M A <muhammedanas6282@gmail.com>
2025-06-13 11:03:53 +0530

    updated

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -89,26 +89,10 @@ WSGI_APPLICATION = 'automax.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if env('DATABASE_NAME', default='') == 'db.sqlite3':
-    # Local SQLite
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
-    # Production PostgreSQL (Render)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': env('DB_NAME'),
-            'USER': env('DB_USER'),
-            'PASSWORD': env('DB_PASSWORD'),
-            'HOST': env('DB_HOST'),
-            'PORT': env('DB_PORT'),
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(default=env('DATABASE_URL'))
+}
+
 
 
 # Password validation
